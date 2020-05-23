62629272d07b460942819f8799cfa91cf2c43018
Veronica Andrade <veronica726@gmail.com>
2020-05-23 00:40:46 -0600

    Create typescript class objects

---
+++ src/app/error-log.ts
@@ -0,0 +1,4 @@
+export class ErrorLog {
+  id: number;
+  error: string;
+}
---
+++ src/app/request-log.ts
@@ -0,0 +1,17 @@
+export class RequestLog {
+  id: number;
+  service: string;
+  version: string;
+  action: string;
+  result: string;
+  created_at: string;
+}
+
+//{
+// "id": 1530312,
+// "service": "code_challenges",
+// "version": "v1",
+// "action": "request_logs_index",
+// "result": "action_successful",
+// "created_at": "2020-05-20T22:04:05.000Z"
+//}
